8eef6b121f0245974983dad2244b6450295ffadd
Bram Oosterlynck <booster@inuits.eu>
2023-05-15 17:58:39 +0200

    Refs #110353 generate manifests from oai pmh interface

--- importers/oai_pmh_importer.py
+++ importers/oai_pmh_importer.py
@@ -1,6 +1,5 @@
 import validators
 
-from datetime import datetime
 from importers.base_importer import BaseImporter
 from models.manifest import Manifest, NoValidManifest
 from oaipmh.client import Client
@@ -20,18 +19,20 @@ class OaiPmhImporter(BaseImporter):
             if validators.url(oai_url):
                 self.clients.append(Client(oai_url, registry))
 
-    def _get_manifest_list(self, client):
-        manifests = list()
-        for record in client.listRecords(metadataPrefix=self.metadata_prefix):
+    def _get_manifests_from_oai(self, client, from_date=None, until_date=None):
+        for record in client.listRecords(
+            metadataPrefix=self.metadata_prefix, from_=from_date, until=until_date
+        ):
             for manifest_url in record[1].getField(self.manifest_field):
                 try:
-                    manifests.append(Manifest.from_url(manifest_url).as_dict())
+                    yield Manifest.from_url(manifest_url).as_dict()
                 except NoValidManifest as ex:
                     print(f"Couldn't parse manifest {manifest_url} because of {ex}")
-        return manifests
 
     def get_manifests(self, from_date=None, until_date=None):
         manifests = list()
         for client in self.clients:
-            manifests.extend(self._get_manifest_list(client))
+            manifests.extend(
+                self._get_manifests_from_oai(client, from_date, until_date)
+            )
         return manifests
