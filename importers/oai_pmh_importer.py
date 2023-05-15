792221af265d318ccd3d7df5d77b2155001986dd
Bram Oosterlynck <booster@inuits.eu>
2023-05-15 14:36:48 +0200

    Refs #111665 only return valid manifests

--- importers/oai_pmh_importer.py
+++ importers/oai_pmh_importer.py
@@ -2,26 +2,36 @@ import validators
 
 from datetime import datetime
 from importers.base_importer import BaseImporter
-from models.manifest_v3 import ManifestV3
+from models.manifest_v3 import ManifestV3, NoValidManifest
 from oaipmh.client import Client
 from oaipmh.metadata import MetadataRegistry
 
 
 class OaiPmhImporter(BaseImporter):
-    def __init__(self, urls, metadata_prefix, reader):
+    def __init__(self, urls, metadata_prefix, reader, manifest_field="manifests"):
         self.clients = list()
         self.urls = urls
         self.metadata_prefix = metadata_prefix
         self.reader = reader
+        self.manifest_field = manifest_field
         registry = MetadataRegistry()
         registry.registerReader(self.metadata_prefix, self.reader)
         for oai_url in self.urls:
             if validators.url(oai_url):
                 self.clients.append(Client(oai_url, registry))
 
+    def _get_manifest_list(self, client):
+        manifests = list()
+        for record in client.listRecords(metadataPrefix=self.metadata_prefix):
+            for manifest_url in record[1].getField(self.manifest_field):
+                try:
+                    manifests.append(ManifestV3.from_url(manifest_url).as_dict())
+                except NoValidManifest as ex:
+                    print(f"Couldn't parse manifest {manifest_url} because of {ex}")
+        return manifests
+
     def get_manifests(self, from_date=None, until_date=None):
+        manifests = list()
         for client in self.clients:
-            for record in client.listRecords(metadataPrefix=self.metadata_prefix):
-                for manifest_url in record[1].getField("manifests"):
-                    manifest = ManifestV3.from_url(manifest_url)
-                    print(manifest.as_dict())
+            manifests.extend(self._get_manifest_list(client))
+        return manifests
--- models/manifest_v3.py
+++ models/manifest_v3.py
@@ -1,17 +1,35 @@
 import json
 import urllib.request
 
+from urllib.error import HTTPError
+
+
 class ManifestV3:
     def __init__(self, manifest):
         self.manifest = manifest
-        
+        self.valid_manifest()
+
     def as_dict(self):
         return self.manifest
 
     @classmethod
     def from_url(cls, manifest_url):
-        if not manifest_url.endswith("manifest.json"):
-            return cls(dict())
-        with urllib.request.urlopen(manifest_url) as url:
-            manifest = json.load(url)
-            return cls(manifest)
+        manifest = dict()
+        try:
+            with urllib.request.urlopen(manifest_url) as url:
+                manifest = json.load(url)
+        except HTTPError as ex:
+            print(f"Couldn't fetch manifest {manifest_url} because of {ex}")
+        return cls(manifest)
+
+    def valid_manifest(self):
+        if (
+            self.manifest.get("@context")
+            != "http://iiif.io/api/presentation/3/context.json"
+        ):
+            raise NoValidManifest("Manifest is not in the valid v3 format")
+
+
+class NoValidManifest(Exception):
+    def __init__(self, message):
+        super().__init__(message)
