ecdeecf2b4e31c9bdd6986754402a96765426f72
Bram Oosterlynck <booster@inuits.eu>
2023-08-07 11:39:42 +0200

    Link manifests with importer entity

--- main.py
+++ main.py
@@ -22,16 +22,40 @@ parser.add_argument("--until_time", type=str, help="End date for OAI parse")
 parser.add_argument("--limit", type=int, help="Limit amount of manifests per importer")
 
 
+def create_entity_for_importer(importer_name):
+    entity = {
+        "type": "importer",
+        "metadata": [
+            {
+                "key": "title",
+                "value": importer_name,
+                "lang": "nl",
+            }
+        ]
+    }
+    return elody_client.add_object("entities", entity)
+
+def get_manifest_importer_relation(importer_entity):
+    return [
+        {
+            "label": "importer",
+            "key": importer_entity.get("_id"),
+            "type": "isIn",
+        }
+    ]
+
 def main():
     args = parser.parse_args()
     from_date = datetime.fromisoformat(args.from_time) if args.from_time else None
     until_date = datetime.fromisoformat(args.until_time) if args.until_time else None
     for importer in importers:
+        importer_entity = create_entity_for_importer(importer.get_importer_name())
         for manifest in importer.get_manifests(
             from_date=from_date, until_date=until_date, limit=args.limit
         ):
             try:
-                elody_client.add_object("entities", manifest.as_elody_entity())
+                entity = elody_client.add_object("entities", manifest.as_elody_entity())
+                elody_client.update_object_relations("entities", entity.get("_id"), get_manifest_importer_relation(importer_entity))
             except NonUniqueException:
                 print(
                     f"Manifest {manifest.get_manifest_id()} is already present in the system"
